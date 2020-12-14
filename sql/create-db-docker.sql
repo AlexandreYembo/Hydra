f9612e3222357f0497ac84bd4ff981487d48ff2d
Alexandre Yembo <alexandreyembo@Alexandres-MacBook-Pro.local>
2020-12-14 23:17:21 +0000

    Update create-db-docker.sql

--- sql/create-db-docker.sql
+++ sql/create-db-docker.sql
@@ -342,3 +342,68 @@ REFERENCES [dbo].[Customer] ([Id])
 GO
 ALTER TABLE [dbo].[Address] CHECK CONSTRAINT [FK_Address_Customer_CustomerID]
 GO
+
+
+USE [master]
+GO
+
+CREATE DATABASE [Hydra.Catalog]
+GO
+
+USE [Hydra.Catalog]
+GO
+
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Products](
+	[Id] [uniqueidentifier] NOT NULL,
+	[CategoryId] [uniqueidentifier] NOT NULL,
+	[Name] [varchar](250) NOT NULL,
+	[Description] [varchar](500) NOT NULL,
+	[Active] [bit] NOT NULL,
+	[Price] [decimal](18, 2) NOT NULL,
+	[CreatedDate] [datetime2](7) NOT NULL,
+	[Image] [varchar](250) NOT NULL,
+	[Qty] [int] NOT NULL,
+	[Height] [int] NULL,
+	[Width] [int] NULL,
+	[Length] [int] NULL
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Products] ADD  CONSTRAINT [PK_Products] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
+CREATE NONCLUSTERED INDEX [IX_Products_CategoryId] ON [dbo].[Products]
+(
+	[CategoryId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Products]  WITH CHECK ADD  CONSTRAINT [FK_Products_Categories_CategoryId] FOREIGN KEY([CategoryId])
+REFERENCES [dbo].[Categories] ([Id])
+ON DELETE CASCADE
+GO
+ALTER TABLE [dbo].[Products] CHECK CONSTRAINT [FK_Products_Categories_CategoryId]
+GO
+
+
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Categories](
+	[Id] [uniqueidentifier] NOT NULL,
+	[Name] [varchar](250) NOT NULL,
+	[Code] [int] NOT NULL
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Categories] ADD  CONSTRAINT [PK_Categories] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
